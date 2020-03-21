6abfe13c356ade0262b384b280a4975b33896ff7
Liam Becker <liambecker@liams-air.home>
2020-03-21 14:49:21 -0400

    added y-values for note positions a3 to f5

--- SceneTemplate.js
+++ SceneTemplate.js
@@ -7,8 +7,8 @@ class SceneTemplate extends Phaser.Scene{
 
     preload() {
         this.load.image('staff', "./assets/staff.png");
-        this.load.image('note-black', "./assets/note-black");
-        this.load.image('note-red', "./assets/nore-red");
+        this.load.image('note-black', "./assets/note-black.png");
+        this.load.image('note-red', "./assets/note-red.png");
     }
 
     create(){
@@ -17,9 +17,22 @@ class SceneTemplate extends Phaser.Scene{
 
         //add background image
         this.add.image(0, 0, 'staff').setOrigin(0).setScale(1);
+
+        //add notes
+        const notes = this.physics.add.group();
+    
+        //TODO make note random?
+        //hard code note for now
+
+        gameState.note_1 = this.physics.add.sprite(x_values.one, y_values.f4, 'note-black').setScale(0.85);
+        gameState.note_2 = this.physics.add.sprite(x_values.two, y_values.a4, 'note-black').setScale(0.85);
+        gameState.note_3 = this.physics.add.sprite(x_values.three, y_values.c5, 'note-black').setScale(0.85);
+        gameState.note_4 = this.physics.add.sprite(x_values.four, y_values.f5, 'note-black').setScale(0.85);
+
+        
     }
 
     update(){
-        
+
     }
 }
--- game.js
+++ game.js
@@ -1,11 +1,38 @@
 const width = 900;
 const height = 500;
+const y_values = {
+    a3: 480,
+    b3: 445,
+    c4: 410,
+    e4: 375, 
+    f4: 340, 
+    g4: 305, 
+    a4: 270,
+    b4: 235,
+    c5: 201,
+    d5: 165,
+    e5: 130,
+    f5: 95
+};
+
+const x_values = {
+    one: 300,
+    two: 450,
+    three: 600,
+    four: 750
+}
 
 const config = {
     width: width,
     height: height,
     backgroundColor: "ffffff",
-    scene: [SceneTemplate]
+    scene: [SceneTemplate],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }
+        }
+    }
 }
 
 const game = new Phaser.Game(config);
